11bdc724c1e21d1aaeed324d01980aabab50c717
Vu Nguyen <vunq.uit@gmail.com>
2022-09-08 21:46:51 +0700

    Generate channel and socket for simple chat app

---
+++ lib/chatter_web/channels/chat_room_channel.ex
@@ -0,0 +1,59 @@
+defmodule ChatterWeb.ChatRoomChannel do
+  use ChatterWeb, :channel
+
+  alias Chatter.Message
+  @impl true
+  def join("chat_room:lobby", payload, socket) do
+    if authorized?(payload) do
+      send(self(), :after_join)
+      {:ok, socket}
+    else
+      {:error, %{reason: "unauthorized"}}
+    end
+  end
+
+  def handle_info(:after_join, socket) do
+    Message.recent_messages()
+    |> Enum.each(fn msg -> push(socket, "new_message", format_msg(msg)) end)
+    {:noreply, socket}
+  end
+
+  defp format_msg(msg) do
+    %{
+      name: msg.name,
+      message: msg.message
+    }
+  end
+  # Channels can be used in a request/response fashion
+  # by sending replies to requests from the client
+  @impl true
+  def handle_in("ping", payload, socket) do
+    {:reply, {:ok, payload}, socket}
+  end
+
+  # It is also common to receive messages from the client and
+  # broadcast to everyone in the current topic (chat_room:lobby).
+  @impl true
+  # My comments
+  # =======================
+  # def handle_in("shout", payload, socket) do
+  #   broadcast(socket, "shout", payload)
+  #   {:noreply, socket}
+  # end
+  # =======================
+  def handle_in("new_message", payload, socket) do
+    spawn(fn -> save_message(payload) end)
+    broadcast! socket, "new_message", payload
+    {:noreply, socket}
+  end
+  
+  defp save_message(message) do
+    Chatter.Message.changeset(%Chatter.Message{}, message)
+      |> Chatter.Repo.insert
+  end
+
+  # Add authorization logic here as required.
+  defp authorized?(_payload) do
+    true
+  end
+end
